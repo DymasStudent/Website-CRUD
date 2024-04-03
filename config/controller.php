298203971b70269b1c5e76155690165a9b102446
Dymas <126073035+DymasStudent@users.noreply.github.com>
2024-04-03 10:40:36 +0700

    tapis

--- config/controller.php
+++ config/controller.php
@@ -16,6 +16,7 @@ function select($query)
  return $rows;
 }
 
+// fungsi menambah data barang
 function create_barang($post)
 {
  global $db;
@@ -23,10 +24,29 @@ function create_barang($post)
  $nama = $post['nama'];
  $jumlah = $post['jumlah'];
  $harga = $post['harga'];
-
+ // query tambah data
  $query = "INSERT INTO barang VALUES(null, '$nama', '$jumlah', '$harga', CURRENT_TIMESTAMP())";
 
  mysqli_query($db, $query);
 
  return mysqli_affected_rows($db);
+}
+
+//fungsi mengubah data barang
+function update_barang($post)
+{
+ global $db;
+
+ $id_barang = $post['id_barang'];
+ $nama = $post['nama'];
+ $jumlah = $post['jumlah'];
+ $harga = $post['harga'];
+
+ // query ubah data
+ $query = "UPDATE barang SET nama = '$nama', jumlah = '$jumlah', harga = '$harga' WHERE id_barang = $id_barang";
+
+ mysqli_query($db, $query);
+
+ return mysqli_affected_rows($db);
+
 }
--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $data_barang = select('SELECT * FROM barang');
   <h1>DATA BARANG</h1>
   <hr>
 
-  <a href="form-tambah.php" class="btn btn-primary mb-1">Tambah</a>
+  <a href="tambah-barang.php" class="btn btn-primary mb-1">Tambah</a>
 
   <table class="table table-bordered table-striped mt-3">
     <thead>
@@ -42,7 +42,7 @@ $data_barang = select('SELECT * FROM barang');
             <?= date('d/m/Y | H:i:s', strtotime($barang['tanggal'])); ?>
           </td>
           <td width="15%" class="text-center">
-            <a href="" class="btn btn-success">Ubah</a>
+            <a href="ubah-barang.php?id_barang=<?= $barang['id_barang']; ?>" class="btn btn-success">Ubah</a>
             <a href="" class="btn btn-danger">Hapus</a>
           </td>
         </tr>
